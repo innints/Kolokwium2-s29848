8f0604c17ca73842775171e1b210363a151fe1a5
s29848 <s29848@pjwstk.edu.pl>
2025-06-03 22:42:01 +0200

    Initialization files

---
+++ Kolokwium2/Kolokwium2/Program.cs
@@ -0,0 +1,33 @@
+using Kolokwium2.Data;
+using Microsoft.EntityFrameworkCore;
+using Kolokwium2.Services;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddScoped<IDbService, DbService>();
+
+builder.Services.AddDbContext<AppDbContext>(
+    opt =>
+    {
+        opt.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+        
+    });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
